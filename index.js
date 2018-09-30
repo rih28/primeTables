d8a53c98ddc81543f6ae1d1b264876b0d25ad990
Russell Hunter <rih28@cam.ac.uk>
2018-09-30 10:54:33 +0100

    Initialisation of the project. Created index.js as main javascript file with a list of //TODO:s from the test plan. made a test folder with a single file called test with a list of //TODO:s for the test plan based on the spec. The app requires mocha (i put in globally) and command-line-args

---
+++ index.js
@@ -0,0 +1,10 @@
+
+//From the description:
+//TODO: Make sure the argument is a positive integer
+//TODO: get a list of primes
+//TOOD: create a multiply matrix
+//TODO: Show the grid
+
+function main() {
+  //TODO: main function to run the program
+}
---
+++ test/test.js
@@ -0,0 +1,4 @@
+
+//TODO: Test user input
+//TODO: Test Prime list
+//TODO: Test Multiply Matrix
