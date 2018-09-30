9267a83b81551e4f286d612450aba4782f5e64eb
Russell Hunter <rih28@cam.ac.uk>
2018-09-30 11:13:49 +0100

    Created a new file for handling user inputs correctness called argumentIsPositiveInteger with a single function that is exporting to make testing easier. Added four new tests in test.js and they have failed due to there only being a code stub in argumentIsPositiveInteger. I also updated package.json as mocha wasn't installed per project (i.e. -g) and needed to be added.

---
+++ argumentIsPositiveInteger.js
@@ -0,0 +1,12 @@
+
+/*
+ * Function for testing whether the user input is viable i.e. a positive integer.
+ * @param {Number} prime
+ * @return {bool}
+ */
+
+function argumentIsPositiveInteger(prime) {
+  return false;
+}
+
+module.exports = argumentIsPositiveInteger;
--- test/test.js
+++ test/test.js
@@ -1,4 +1,34 @@
+var assert = require('assert');  //for mocha unit testing framework
+var argumentIsPositiveInteger = require('../argumentIsPositiveInteger');
 
-//TODO: Test user input
+//Create a test collection for the user input
+describe('UserInput', function() {
+  //Tests what happens when the input is not a Number
+  describe('#primesInputArgument', function() {
+    it('should return false when the input is not a Number', function() {
+      assert.equal(argumentIsPositiveInteger("a"), false);
+    });
+  });
+  //Tests what happens when the input is not an Integer
+  describe('#primesInputArgument', function() {
+    it('should return false when the input is a Number but not an Integer', function() {
+      assert.equal(argumentIsPositiveInteger(1.5), false);
+    });
+  });
+  //Tests what happens when the number is not a positive integer
+  describe('#primesInputArgument', function() {
+    it('should return false when the input is a Number and an Integer but not positive', function() {
+      assert.equal(argumentIsPositiveInteger(-1), false);
+    });
+  });
+  //Tests what happens when the number is a positive Integer
+  describe('#primesInputArgument', function() {
+    it('should return true when the input is a positive integer', function() {
+      assert.equal(argumentIsPositiveInteger(1), true);
+    });
+  });
+});
+
+
 //TODO: Test Prime list
 //TODO: Test Multiply Matrix
